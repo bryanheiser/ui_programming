1e7b5e4e052243b657c0981ad52ffe064c8a67fb
Heiser <bryan.heiser@level3.com>
2019-02-21 11:34:26 -0700

    base code

---
+++ calculator.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$("#dialer_content").show();
+	$("#contact_list_content").hide();
+	$("#add_contact_content").hide();
+	$("#help_content").hide();
+});
+
+$("#numPad button").click(function() {
+	$("#enterNumber").val($("#enterNumber").val() + this.innerText);
+});
+
+$("#clear_id").click(function() {
+	$("#enterNumber").val("");
+});
+
+document.addEventListener("keydown", function ( event ) {
+	if ( event.shiftKey ) {
+		if ( event.which == 187 ) {
+			$("#enterNumber").val($("#enterNumber").val() + '+');
+		}
+		else if ( event.which == 189 ) {
+			$("#enterNumber").val($("#enterNumber").val() + '-');
+		}
+		else if ( event.which == 56 ) {
+			$("#enterNumber").val($("#enterNumber").val() + '*');
+		}
+	}
+	else if ( event.which == 191 ) {
+		$("#enterNumber").val($("#enterNumber").val() + '/');
+	}
+	else if ( event.which == 68 ) {
+		$("#enterNumber").val("");
+	}
+	else if ( event.which >= 48 && event.which <= 57 ) {
+		var entry = event.which - 48;
+		$("#enterNumber").val($("#enterNumber").val() + entry.toString());
+	}
+});
